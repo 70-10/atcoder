b8344253528d0f77f765b8f0b63d752db78f2771
70_10 <7010.dev@gmail.com>
2022-09-19 13:05:28 +0900

    [fix] typescript: maa_031

--- typescript/problems/math-and-algorithm/maa_031/logic.ts
+++ typescript/problems/math-and-algorithm/maa_031/logic.ts
@@ -5,20 +5,22 @@ export const logic = (arg: string) => {
   const dp2: number[] = [];
 
   for (let i = 0; i <= nums.length; i++) {
-    if (i === 0) {
-      dp1[i] = 0;
-      dp2[i] = 0;
-      continue;
+    switch (i) {
+      case 0: {
+        dp1[i] = 0;
+        dp2[i] = 0;
+        break;
+      }
+      case 1: {
+        dp1[i] = nums[i - 1];
+        dp2[i] = 0;
+        break;
+      }
+      default: {
+        dp1[i] = dp2[i - 1] + nums[i - 1];
+        dp2[i] = Math.max(dp1[i - 1], dp2[i - 1]);
+      }
     }
-
-    if (i === 1) {
-      dp1[i] = nums[i - 1];
-      dp2[i] = 0;
-      continue;
-    }
-
-    dp1[i] = dp2[i - 1] + nums[i - 1];
-    dp2[i] = Math.max(dp1[i - 1], dp2[i - 1]);
   }
 
   return Math.max(dp1[dp1.length - 1], dp2[dp2.length - 1]);
